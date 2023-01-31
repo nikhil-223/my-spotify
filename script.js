7f8a52a5352e27ad8622b9ab89c6000526c80922
nikhil-223 <113900755+nikhil-223@users.noreply.github.com>
2023-01-31 10:31:08 +0530

    updated

--- script.js
+++ script.js
@@ -1,70 +1,81 @@
-
-
 let songlist = [
 	{
 		songName: "Aise Kyun ",
 		filePath: "songs/Aise Kyun .mp3",
 		coverPath: "./covers/1.jpg",
+		songtime: "5:45",
 	},
 	{
 		songName: "Gul",
 		filePath: "songs/Anuv Jain.mp3",
 		coverPath: "./covers/2.jpg",
+		songtime: "5:45",
 	},
 	{
 		songName: "Flute",
 		filePath: "songs/flute.mp3",
 		coverPath: "./covers/3.jpg",
+		songtime: "5:45",
 	},
 	{
 		songName: "Haaye Oye",
 		filePath: "songs/Haaye Oye.mp3",
 		coverPath: "./covers/4.jpg",
+		songtime: "5:45",
 	},
 	{
 		songName: "Iraaday Song Abdul Hannan",
 		filePath: "songs/Iraaday Song Abdul Hannan.mp3",
 		coverPath: "./covers/5.jpg",
+		songtime: "5:45",
 	},
 	{
 		songName: "Kuch na kari",
 		filePath: "songs/kuch na kari.mp3",
 		coverPath: "./covers/6.jpg",
+		songtime: "5:45",
 	},
 	{
 		songName: "Raataan Lambiyan",
 		filePath: "songs/Raataan Lambiyan.mp3",
 		coverPath: "./covers/7.jpg",
+		songtime: "5:45",
 	},
 	{
 		songName: "Ranjha",
 		filePath: "songs/Ranjha.mp3",
 		coverPath: "./covers/8.jpg",
+		songtime: "5:45",
 	},
 	{
 		songName: "Yeh Saari Baat",
 		filePath: "songs/Yeh Saari Baat.mp3",
 		coverPath: "./covers/9.jpg",
+		songtime: "5:45",
 	},
 	{
 		songName: "Flute music",
 		filePath: "songs/flute music.mp3",
 		coverPath: "./covers/10.jpg",
+		songtime: "5:45",
 	},
 	{
 		songName: "Flute music",
 		filePath: "songs/flute music.mp3",
 		coverPath: "./covers/10.jpg",
+		songtime: "5:45",
 	},
 	{
 		songName: "Flute music",
 		filePath: "songs/flute music.mp3",
 		coverPath: "./covers/10.jpg",
+		songtime: "5:45",
 	},
 	{
 		songName: "Flute music",
 		filePath: "songs/flute music.mp3",
 		coverPath: "./covers/10.jpg",
+		songtime: "5:45",
 	},
 ];
 let gif = document.getElementById("gif");
@@ -72,87 +83,87 @@ let masterplay = document.getElementById("masterplay");
 let progressBar = document.getElementById("myProgressBar");
 let songItems = Array.from(document.getElementsByClassName("songItem"));
 let songPlay = new Audio(`./songs/Aise Kyun .mp3`);
-let forwardbtn= document.getElementById("forwardd")
-let backwardbtn= document.getElementById("backwardd")
-let bottom_songname = document.getElementsByClassName("itemInfo")[0].getElementsByTagName("span")[0];
-let filepath=0;
-let filename="";
-if(screen.width<=500){
-   bottom_songname.innerText="";
+let forwardbtn = document.getElementById("forwardd");
+let backwardbtn = document.getElementById("backwardd");
+let bottom_songname = document
+	.getElementsByClassName("itemInfo")[0]
+	.getElementsByTagName("span")[0];
+let filepath = 0;
+let filename = "";
+if (screen.width <= 500) {
+	bottom_songname.innerText = "";
 }
 
 songItems.forEach((element, i) => {
 	element.getElementsByTagName("img")[0].src = songlist[i].coverPath;
 	element.getElementsByTagName("span")[0].innerText = songlist[i].songName;
+	element
+		.querySelector(".songListPlay")
+		.getElementsByTagName("span")[0].innerText = songlist[i].songtime;
 });
 
-forwardbtn.addEventListener("click",()=>{
-		if(filepath>9){
-			filepath=0;
-		}
-		else{
-			filepath+=1;
-		}
-		filename = songlist[filepath].songName;
-		if (screen.width <= 500) {
-			bottom_songname.innerText = "";
-		} else {
-			bottom_songname.innerText = filename;
-		}
-		songPlay.src = songlist[filepath].filePath;
-		songPlay.currentTime = 0;
-		songPlay.play();
-		masterplay.classList.add("fa-pause");
-		masterplay.classList.remove("fa-play");
-		gif.style.opacity = 1;
-})
-backwardbtn.addEventListener("click",()=>{
-		if(filepath<0){
-			filepath=9;
-		}
-		else{
-			filepath-=1;
-		}
-		filename = songlist[filepath].songName;
-		if (screen.width <= 500) {
-			bottom_songname.innerText = "";
-		} else {
-			bottom_songname.innerText = filename;
-		}
-		songPlay.src = songlist[filepath].filePath;
-		songPlay.currentTime = 0;
-		songPlay.play();
-		masterplay.classList.add("fa-pause");
-		masterplay.classList.remove("fa-play");
-		gif.style.opacity = 1;
-})
+forwardbtn.addEventListener("click", () => {
+	if (filepath > 9) {
+		filepath = 0;
+	} else {
+		filepath += 1;
+	}
+	filename = songlist[filepath].songName;
+	if (screen.width <= 500) {
+		bottom_songname.innerText = "";
+	} else {
+		bottom_songname.innerText = filename;
+	}
+	songPlay.src = songlist[filepath].filePath;
+	songPlay.currentTime = 0;
+	songPlay.play();
+	masterplay.classList.add("fa-pause");
+	masterplay.classList.remove("fa-play");
+	gif.style.opacity = 1;
+});
+backwardbtn.addEventListener("click", () => {
+	if (filepath < 0) {
+		filepath = 9;
+	} else {
+		filepath -= 1;
+	}
+	filename = songlist[filepath].songName;
+	if (screen.width <= 500) {
+		bottom_songname.innerText = "";
+	} else {
+		bottom_songname.innerText = filename;
+	}
+	songPlay.src = songlist[filepath].filePath;
+	songPlay.currentTime = 0;
+	songPlay.play();
+	masterplay.classList.add("fa-pause");
+	masterplay.classList.remove("fa-play");
+	gif.style.opacity = 1;
+});
 
-songItems.forEach((element,i) => {
+songItems.forEach((element, i) => {
 	element.addEventListener("click", (e) => {
-
-        filepath =parseInt(e.target.id) ;
-        songPlay.src = songlist[filepath].filePath;
+		filepath = parseInt(e.target.id);
+		songPlay.src = songlist[filepath].filePath;
 		filename = songlist[filepath].songName;
 		if (screen.width <= 500) {
 			bottom_songname.innerText = "";
 		} else {
 			bottom_songname.innerText = filename;
 		}
-        songPlay.currentTime=0;
-        songPlay.play();
-        masterplay.classList.add("fa-pause");
-        masterplay.classList.remove("fa-play");
-        gif.style.opacity = 1;
-        
-    });
+		songPlay.currentTime = 0;
+		songPlay.play();
+		masterplay.classList.add("fa-pause");
+		masterplay.classList.remove("fa-play");
+		gif.style.opacity = 1;
+	});
 });
 
 masterplay.addEventListener("click", () => {
 	filename = songlist[filepath].songName;
 	if (screen.width <= 500) {
 		bottom_songname.innerText = "";
-	}
-	else{
+	} else {
 		bottom_songname.innerText = filename;
 	}
 
